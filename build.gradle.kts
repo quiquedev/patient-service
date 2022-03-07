73a255861650576edd25c5a95b78ff89a3648549
Enrique Molina <enrique.molina.ext@lendico.de>
2022-03-07 15:55:48 +0100

    :D

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.2"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
+    id("org.springframework.boot") version "2.4.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
     id("nu.studer.jooq") version "7.1.1"
 }
 
@@ -13,26 +13,27 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 extra["testcontainersVersion"] = "1.16.2"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-jooq")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("org.liquibase:liquibase-core")
-	runtimeOnly("mysql:mysql-connector-java")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:mysql")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.liquibase:liquibase-core")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
+    runtimeOnly("mysql:mysql-connector-java")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:mysql")
     jooqGenerator("jakarta.xml.bind:jakarta.xml.bind-api:4.0.0-RC3")
     jooqGenerator("org.jooq:jooq-meta-extensions-liquibase")
     jooqGenerator("org.liquibase:liquibase-core")
@@ -41,20 +42,20 @@ dependencies {
 
 
 dependencyManagement {
-	imports {
-		mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
-	}
+    imports {
+        mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
+    }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 jooq {
--- src/main/kotlin/info/quiquedev/patientservice/patients/usecases/PatientsUseCases.kt
+++ src/main/kotlin/info/quiquedev/patientservice/patients/usecases/PatientsUseCases.kt
@@ -1,16 +1,114 @@
 package info.quiquedev.patientservice.patients.usecases
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import info.quiquedev.patientservice.patients.NewPatientDto
 import info.quiquedev.patientservice.patients.PatientDto
+import info.quiquedev.patientservice.patients.ReactorUtils.safeMono
+import info.quiquedev.patientsservice.patients.usecases.tables.references.PATIENTS
 import org.jooq.DSLContext
 import reactor.core.publisher.Mono
 import java.time.Clock
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.ZoneOffset.UTC
+import java.util.Optional
+import java.util.UUID
+import java.util.concurrent.CompletionStage
 
 class PatientsUseCases(
     private val dsl: DSLContext,
     private val clock: Clock
 ) {
-    fun createPatient(newPatientDto: NewPatientDto): Mono<PatientDto> = TODO()
+
+    sealed class PatientsUsecasesError(message: String, throwable: Throwable? = null) : Throwable(message, throwable)
+    data class ExistingPassportNumberError(val passportNumber: String) :
+        PatientsUsecasesError("existing passport number $passportNumber")
+
+    data class UnexpectedError(val throwable: Throwable) :
+        PatientsUsecasesError("unexpected error", throwable)
+
+    data class TooManyPatientsError(val id: String, val amount: Int) :
+        PatientsUsecasesError("$amount patients found for id $id")
+
+    data class PatientNotFoundError(val id: String) :
+        PatientsUsecasesError("patient with id $id not found")
+
+    fun createPatient(newPatientDto: NewPatientDto):
+            Mono<PatientDto> =
+        Mono
+            .fromFuture {
+                dsl.transactionResultAsync {
+                    if (existsPassportNumber(newPatientDto))
+                        throw ExistingPassportNumberError(newPatientDto.passportNumber)
+                    val id = UUID.randomUUID().toString()
+                    val createdAt = Instant.now(clock)
+
+                    storeNewPatient(id, createdAt, newPatientDto)
+
+                    PatientDto(
+                        id = id,
+                        name = newPatientDto.name,
+                        surname = newPatientDto.surname,
+                        passportNumber = newPatientDto.passportNumber,
+                        createdAt = createdAt
+                    )
+                }.toCompletableFuture()
+            }
+            .onErrorMap {
+                when (it) {
+                    is ExistingPassportNumberError -> it
+                    else -> UnexpectedError(it)
+                }
+            }
+
+    fun findPatient(id: String): Mono<Optional<PatientDto>> =
+        safeMono {
+            val patients = dsl
+                .selectFrom(PATIENTS)
+                .where(PATIENTS.ID.eq(id))
+                .toSet()
+
+            when (patients.size) {
+                0 -> Optional.empty()
+                1 -> {
+                    val patient = patients.first()
+                    val patientDto = PatientDto(
+                        id = patient.id!!,
+                        name = patient.name!!,
+                        surname = patient.surname!!,
+                        passportNumber = patient.passportNumber!!,
+                        createdAt = patient.createdAt!!.toInstant(UTC)
+                    )
+                    Optional.of(patientDto)
+                }
+                else -> throw TooManyPatientsError(id, patients.size)
+            }
+        }
 
 
+    private fun storeNewPatient(
+        id: String,
+        createdAt: Instant?,
+        newPatientDto: NewPatientDto
+    ) {
+        val newPatient = dsl.newRecord(PATIENTS)
+        newPatient.id = id
+        newPatient.createdAt = LocalDateTime.ofInstant(createdAt, ZoneId.of("UTC"))
+        newPatient.name = newPatientDto.name
+        newPatient.surname = newPatientDto.surname
+        newPatient.passportNumber = newPatientDto.passportNumber
+        newPatient.store()
+    }
+
+    private fun existsPassportNumber(newPatientDto: NewPatientDto) =
+        dsl.select(PATIENTS.ID)
+            .from(PATIENTS)
+            .where(PATIENTS.PASSPORT_NUMBER.eq(newPatientDto.passportNumber))
+            .fetch()
+            .toSet()
+            .isNotEmpty()
 }

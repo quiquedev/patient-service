90b0fba239e5edb9c61821787febecb6353fc34f
Enrique Molina <enrique.molina.ext@lendico.de>
2022-03-12 13:18:27 +0100

    :D

--- src/test/kotlin/info/quiquedev/patientservice/patients/usecases/PatientsUseCasesTest.kt
+++ src/test/kotlin/info/quiquedev/patientservice/patients/usecases/PatientsUseCasesTest.kt
@@ -2,7 +2,11 @@ package info.quiquedev.patientservice.patients.usecases
 
 import info.quiquedev.patientservice.patients.ExistingPassportNumberError
 import info.quiquedev.patientservice.patients.NewPatientDto
+import info.quiquedev.patientservice.patients.PatientDto
 import info.quiquedev.patientservice.patients.usecases.PatientsUseCasesTest.TestValues.NEW_PATIENT_DTO
+import info.quiquedev.patientservice.patients.usecases.PatientsUseCasesTest.TestValues.NEW_PATIENT_RECORD
+import info.quiquedev.patientservice.patients.usecases.PatientsUseCasesTest.TestValues.PATIENT_DTO
+import info.quiquedev.patientsservice.patients.usecases.tables.references.PATIENTS
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
@@ -12,6 +16,10 @@ import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
+import java.time.Instant
+import java.time.Instant.now
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 @ExtendWith(MockitoExtension::class)
 class PatientsUseCasesTest {
@@ -44,12 +52,50 @@ class PatientsUseCasesTest {
             .verify()
     }
 
+    @Test
+    fun `create patient should create patient`() {
+        `when`(
+            repository.createPatient(
+                name = NEW_PATIENT_DTO.name,
+                surname = NEW_PATIENT_DTO.surname,
+                passportNumber = NEW_PATIENT_DTO.passportNumber
+            )
+        )
+            .thenReturn(
+                Mono.just(NEW_PATIENT_RECORD)
+            )
+
+        StepVerifier
+            .create(useCases.createPatient(NEW_PATIENT_DTO))
+            .expectNext(PATIENT_DTO)
+            .verifyComplete()
+    }
+
+
     object TestValues {
         val NEW_PATIENT_DTO = NewPatientDto(
             name = "marcel",
             surname = "lineal",
             passportNumber = "12345687II"
         )
+
+        val PATIENT_DTO = PatientDto(
+            id = "91ecd50b-b035-46f2-9ba7-8ce99ae33e17",
+            name = NEW_PATIENT_DTO.name,
+            surname = NEW_PATIENT_DTO.surname,
+            passportNumber = NEW_PATIENT_DTO.passportNumber,
+            createdAt = now(FIXED_CLOCK)
+        )
+
+        val NEW_PATIENT_RECORD = let {
+            val record = TEST_DSL.newRecord(PATIENTS)
+            record.id = "91ecd50b-b035-46f2-9ba7-8ce99ae33e17"
+            record.name = PATIENT_DTO.name
+            record.surname = PATIENT_DTO.surname
+            record.passportNumber = PATIENT_DTO.passportNumber
+            record.createdAt = LocalDateTime.ofInstant(PATIENT_DTO.createdAt, ZoneId.of("UTC"))
+            record
+        }
     }
 
 }
--- src/test/kotlin/info/quiquedev/patientservice/patients/usecases/TestUtils.kt
+++ src/test/kotlin/info/quiquedev/patientservice/patients/usecases/TestUtils.kt
@@ -1,5 +1,7 @@
 package info.quiquedev.patientservice.patients.usecases
 
+import org.jooq.impl.DSL
+import org.jooq.tools.jdbc.MockConnection
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -7,7 +9,7 @@ import java.time.Clock
 import java.time.Instant
 import java.time.ZoneId
 
-val fixedClock: Clock = Clock.fixed(
+val FIXED_CLOCK: Clock = Clock.fixed(
     Instant.ofEpochMilli(1607609508000),
     ZoneId.of("Europe/Berlin")
 )
@@ -16,5 +18,7 @@ val fixedClock: Clock = Clock.fixed(
 class FixedClockConfig {
     @Bean
     @Primary
-    fun fixedClock() = fixedClock
+    fun fixedClock() = FIXED_CLOCK
 }
+
+val TEST_DSL = DSL.using(MockConnection { emptyArray() })
